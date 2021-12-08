cd2a0adbc9280551f5e1beefa1daf12b166b9111
J David Smith <emallson@emallson.net>
2021-12-08 00:55:52 -0500

    initial commit
    includes a lot of laravel junk but i'm not 100% on what i can safely
    remove (yet) so it stays

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
+
+const appName = "WCL Quick View";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        return render(<App {...props} />, el);
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
